b6e8becdf7849656dae2f32d298c29782d06e64c
Patricia Wheeler <pgwheeler@cox.net>
2017-05-17 02:25:00 +0000

    multi page form

---
+++ part-01/index.php
@@ -0,0 +1,38 @@
+<?php
+	include 'quiz.php';
+
+?>
+
+
+
+
+<DOCTYPE <!DOCTYPE html>
+<html>
+    <head>
+       <link rel="stylesheet" href="style.css" />
+       <title>"Design your Window"</title>
+    </head>
+    <body>
+    	<div class="question">
+    	<h5>3 QUESTIONS</h5>
+    	<hr class="hr1">
+    	
+    </div>
+    	
+	    <div class="quiz-title" </div>
+	    <h1>How to dress-up a Window quiz.</h1>
+	    
+	    <div class="subtitle">
+		<h3>Take this quiz to see how much you know!</h3>
+	    </div>
+
+    <div class="sbutton">
+		<a href="question1.php" style="text-decoration:none;">START</a>
+		
+       
+	</div>
+	
+	
+
+    </body>
+</html>
---
+++ part-01/quiz.php
@@ -0,0 +1,17 @@
+<?php 
+	$quiz = array(
+		0 => "What items will you need to start?<br>A. scissors B. pins C. Sewing machine D. All the above",
+		1 => "What measurement will you need?<br>A. length B. width C. Both  D. neither",
+		2 => "How much time will it take?<br>A. 4 hours B. 1 hour  C. All day D. 1 Day",
+		3 => "What Machine is best? A. Sewing Machine B. Both A&B C. hand-stitch D. Serger",
+		);
+
+	$answers = array(
+		0 => "D",
+		1 => "C",
+		2 => "A",
+		3 => "B",
+		);
+
+
+?>
